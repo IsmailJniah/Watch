5474f465de44ffe5bb4226fe5b2ba1796fcb0fca
ismail <ismajniah@gmail.com>
2019-12-26 17:38:14 +0100

    Reloj

---
+++ Watch/Watch/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Watch
+//
+//  Created by strongcio on 26/12/2019.
+//  Copyright © 2019 URJC. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
